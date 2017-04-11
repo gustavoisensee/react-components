d99d7e8e0fd9c51b27ab0be0a8b50155312794c5
Gustavo Isensee <gustavo.isensee@gmail.com>
2017-04-11 11:37:34 -0300

    I've finished the first tests.

--- src/__tests__/question_1.2.js
+++ src/__tests__/question_1.2.js
@@ -1,5 +1,20 @@
-describe('xxx', () => {
-  it('xxx', () => {
-    expect(true).toEqual(true);
+const sameXOAmount = (str) => {
+  const xRegex = str.match(/(x)/ig);
+  const oRegex = str.match(/(o)/ig);
+  const xLength = (xRegex ? xRegex.length : 0);
+  const oLength = (oRegex ? oRegex.length : 0);
+  return (xLength === oLength);
+};
+
+describe('sameXOAmount()', () => {
+  it('should return `true` if number of `X` and `O` is equal', () => {
+    expect(sameXOAmount('xXoO')).toEqual(true);
+    expect(sameXOAmount('aAxXXbBoOo')).toEqual(true);
+    expect(sameXOAmount('abc')).toEqual(true);
+  });
+  it('should return `false` if number of `X` and `O` is not equal', () => {
+    expect(sameXOAmount('OaAxXbBoO')).toEqual(false);
+    expect(sameXOAmount('xgXoXsdxOxz')).toEqual(false);
+    expect(sameXOAmount('aaAmmMxMM')).toEqual(false);
   });
 });
--- src/__tests__/question_1.3.js
+++ src/__tests__/question_1.3.js
@@ -1,5 +1,18 @@
-describe('xxx', () => {
-  it('xxx', () => {
-    expect(true).toEqual(true);
+const findOddAmount = (numbers) => {
+  const array = numbers.reduce((prev, curr) => {
+    const copyPrev = Object.assign({}, prev);
+    copyPrev[curr] = (1 + copyPrev[curr] || 1);
+    return copyPrev;
+  }, {});
+  const odd = Object.keys(array).find(item => array[item] % 2 !== 0);
+
+  return (parseInt(odd, 10) || 0);
+};
+
+describe('findOddAmount()', () => {
+  it('should return the number which occurs with an odd frequency', () => {
+    expect(findOddAmount([1, 2, 2, 3, 3])).toEqual(1);
+    expect(findOddAmount([8, 8, 7, 7, 7])).toEqual(7);
+    expect(findOddAmount([10, 3, 3, 10, 6, 10, 6, 1, 1])).toEqual(10);
   });
 });
