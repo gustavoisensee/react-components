7ace880d5124bd1391db79bcccda563655d839bd
Gustavo Isensee <gustavo.isensee@gmail.com>
2017-04-13 18:54:49 -0300

    Fixed bug on radiobutton presenter.

--- src/modules/main/presenter/radiobutton/reducer.js
+++ src/modules/main/presenter/radiobutton/reducer.js
@@ -1,23 +1,15 @@
 import {
-  CHANGE_RADIOBUTTON_PRESENTER,
-  BUTTONS
+  CHANGE_RADIOBUTTON_PRESENTER
 } from './constant';
 
 const init = {
-  button: BUTTONS
+  button: 1
 };
 
 const radioButtonPresenter = (state = init, action) => {
   switch (action.type) {
     case CHANGE_RADIOBUTTON_PRESENTER: {
-      const buttons = Object.assign([], state.button);
-      for (let i = 0; i < buttons.length; i += 1) {
-        buttons[i].selected = false;
-        if (buttons[i].id === action.id) {
-          buttons[i].selected = true;
-        }
-      }
-      return Object.assign({}, state, { button: buttons });
+      return Object.assign({}, state, { button: action.id });
     }
     default: {
       return Object.assign({}, state);
