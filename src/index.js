65589098987102f710618d2a2220edb80271b10f
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-02-11 00:16:48 +0100

    add storybook as main start, fix index.js App reference

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+const App = () => (<div>Index</div>)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
