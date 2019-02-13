fcd357d3d85844c04908965886404c59a4bda762
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-02-13 18:59:29 +0100

    test with Procfile storybook deploy

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'); 
-const app = express();
-const port = process.env.PORT || 9001;
-
-app.use(express.static(__dirname + '/storybook-static'));
-
-app.listen(port, () => console.log('Listening to port', port));
